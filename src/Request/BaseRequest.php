2362f5eea1b3d56aebbbe4476cbd36e9496f473b
Ashraf Hefny <ashrafhefny@bokdoc.com>
2018-12-12 13:24:23 +0200

    Add support for multiple senders

--- src/Request/BaseRequest.php
+++ src/Request/BaseRequest.php
@@ -62,4 +62,18 @@ abstract class BaseRequest
             'json' => $this->buildBody(),
         ];
     }
+
+    /**
+     * Overwrites the server_key and sender_id that is in the .env file.
+     * This allows for multiple senders. If your application supports multiple users/clients, and each client sends its
+     * own notifications, each one has it's own sender_id (and key), so you can save it in the database.
+     *
+     * @param $server_key
+     * @param $sender_id
+     */
+    public function overwriteServerKeyAndSenderId($server_key, $sender_id)
+    {
+        $this->config['server_key'] = $server_key;
+        $this->config['sender_id'] = $sender_id;
+    }
 }
