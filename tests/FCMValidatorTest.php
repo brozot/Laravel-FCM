903d5550cbc9f3626eb79f28b30c597ea4b34d8b
William Desportes <williamdes@wdes.fr>
2020-10-08 17:32:13 +0200

    Add a test for FCMValidator

---
+++ tests/FCMValidatorTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
+use LaravelFCM\Validator\FCMValidator;
+
+class FCMValidatorTest extends FCMTestCase {
+
+    public function testValidateToken()
+    {
+        $response = new Response(
+            200,
+            [],
+            json_encode([])
+        );
+
+        $client = Mockery::mock(Client::class);
+        $client->shouldReceive('request')->once()->andReturn($response);
+
+        $logger = new \Monolog\Logger('test');
+        $logger->pushHandler(new \Monolog\Handler\NullHandler());
+
+        $fcm = new FCMValidator($client, 'http://test.test', $logger);
+        $validated = $fcm->validateToken('token1');
+
+        $this->assertTrue($validated);
+    }
+}
