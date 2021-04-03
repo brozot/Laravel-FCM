d6ffc359fc1159427f7cdd43e3b7be9d52fc6c5e
William Desportes <williamdes@wdes.fr>
2021-04-03 17:35:06 +0200

    Upgrade Doctum and improve script

--- scripts/make-documentation.sh
+++ scripts/make-documentation.sh
@@ -3,23 +3,24 @@
 set -e
 
 REPOSITORY_ROOT="$(dirname $0)/../"
-BUILD_ROOT="${REPOSITORY_ROOT}/build"
+BUILD_ROOT="$(realpath ${REPOSITORY_ROOT}/build)"
 
 function downloadRelease() {
     rm -f "${BUILD_ROOT}/doctum.phar"
-    curl -# -o "${BUILD_ROOT}/doctum.phar" -O https://doctum.long-term.support/releases/5.1/doctum.phar
+    curl -# -o "${BUILD_ROOT}/doctum.phar" -O https://doctum.long-term.support/releases/5/doctum.phar
+    chmod +x "${BUILD_ROOT}/doctum.phar"
 }
 
 function checkRelease() {
     if [ -f "${BUILD_ROOT}/doctum.phar" ]; then
-        curl -s -o "${BUILD_ROOT}/doctum.phar.sha256" -O https://doctum.long-term.support/releases/5.1/doctum.phar.sha256
+        curl -s -o "${BUILD_ROOT}/doctum.phar.sha256" -O https://doctum.long-term.support/releases/5/doctum.phar.sha256
         cd "${BUILD_ROOT}/"
         sha256sum --check --strict doctum.phar.sha256
         cd - > /dev/null
         if [ "$?" != "0" ]; then
             downloadRelease
         else
-            echo 'You are using the latest 5.1 release of Doctum.'
+            echo 'You are using the latest 5.x.x release of Doctum.'
         fi
     else
         downloadRelease
@@ -27,12 +28,18 @@ function checkRelease() {
 }
 
 function buildDocumentation() {
-    php "${BUILD_ROOT}/doctum.phar" update --ignore-parse-errors -vvv --force "${REPOSITORY_ROOT}/scripts/doctum.php"
+    "${BUILD_ROOT}/doctum.phar" update --ignore-parse-errors -vvv --force "${REPOSITORY_ROOT}/scripts/doctum.php"
     find "${REPOSITORY_ROOT}doc" -type f -name ".delete-me" -delete
     rm "${REPOSITORY_ROOT}doc/renderer.index"
     rm "${REPOSITORY_ROOT}doc/PROJECT_VERSION"
     rm "${REPOSITORY_ROOT}doc/DOCTUM_VERSION"
 }
+
+echo "Using build root: ${BUILD_ROOT}"
+
+if [ ! -d ${BUILD_ROOT} ]; then
+    mkdir ${BUILD_ROOT}
+fi
 
 checkRelease
 
